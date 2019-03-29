ef6359c19a17a181ae44d6026b5bc2a19746aa28
evillt <ijoec123@gmail.com>
2019-03-29 14:38:06 +0800

    feat: docs for gh-pages

--- dvan.config.js
+++ dvan.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 
 module.exports = {
   entry: 'src/index.js',
+  output: {
+    dir: 'docs'
+  },
   chainWebpack(config) {
     config.resolve.alias.set('@comps', path.resolve(__dirname, 'src/comps'))
 

98b00b4f974ca547054b9fd7aa5766f6118c53b2
evillt <ijoec123@gmail.com>
2019-03-29 14:42:46 +0800

    update

--- dvan.config.js
+++ dvan.config.js
@@ -3,7 +3,8 @@ const path = require('path')
 module.exports = {
   entry: 'src/index.js',
   output: {
-    dir: 'docs'
+    dir: 'docs',
+    publicUrl: '/linkage/'
   },
   chainWebpack(config) {
     config.resolve.alias.set('@comps', path.resolve(__dirname, 'src/comps'))
